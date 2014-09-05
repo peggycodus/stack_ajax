b1158f6b7df17e32ff24378ba9f6e8a463e1a49d
Michael Derfler & Peggy Condon <student@epicodus.com>
2014-09-05 13:53:15 -0700

    add edit and delete buttons to question show

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ActiveRecord::Base
   validates :a_question, presence: true
   belongs_to :user
-  has_many :answers, as: :answerable
+  has_many :answers, as: :answerable, dependent: :destroy
 end
